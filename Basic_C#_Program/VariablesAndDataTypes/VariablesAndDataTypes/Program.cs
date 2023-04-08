c502af2c712d0e60cec2660fdf3ad6de0b1f665b
ahart8 <121808111+ahart8@users.noreply.github.com>
2023-04-08 07:31:54 -0400

    I am checking in assignments

---
+++ Basic_C#_Program/VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("The Techacademy \nStudent Daily Report\n\n");
+         Console.WriteLine("What is your Name\n");
+         string yourName = Console.ReadLine();
+         Console.WriteLine("Your name is: " + yourName);
+         
+
+        Console.WriteLine("What course are you on?\n");
+        string theCourse = Console.ReadLine();
+
+        
+        Console.WriteLine("What pageNumber are you on?\n");
+        string pageNumber = Console.ReadLine();
+
+        Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"\n");
+        string needHelp = Console.ReadLine();
+
+
+
+
+        //int favNum = Convert.ToInt32(favoriteNumber);
+        //int total = favNum + 5;
+        //Console.WriteLine("Your favorite number is plus 5: " + total);
+        //Console.ReadLine();
+
+        //bool isStudying = false;
+        //byte hoursWorked = 42;
+        //sbyte currentTempurature= -23;
+        //char questionMark = '\u2103';
+        //Console.WriteLine(questionMark);
+        //Console.ReadLine();
+        //decimal moneyInBank = 100.5m;
+        //double heightInCentimeters = 211.302032095;
+        //float secondLeft = 2.6f;
+        //short temperatureOnMars = -341;
+        //string myName = "Amy";
+
+        //int currentAge = 51;
+        //string yearsOld = currentAge.ToString();
+
+        //bool isRaining = true;
+        //string rainingStatus = Convert.ToString(isRaining);
+        //Console.WriteLine(rainingStatus);
+        //Console.ReadLine();
+
+    }
+}
+
---
+++ DailyReportAssignment/DailyReportAssignment/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //DISPLAY DR HEADING
+        Console.WriteLine("The Techacademy \nStudent Daily Report\n\n");
+        
+        //ASK NAME AND STORE AS A STRING
+        Console.WriteLine("What is your Name\n");
+        string yourName = Console.ReadLine();
+              
+        //ASK COURSE AND STORE AS A STRING
+        Console.WriteLine("What course are you on?\n");
+        string theCourse = Console.ReadLine();
+  
+
+        //ASK PAGE AND STORE AS AND STORE AS AN INT
+        Console.WriteLine("What pageNumber are you on?\n");
+        string pageNumber = Console.ReadLine();
+        int pgNum = Convert.ToInt32(pageNumber);
+
+        //ASK IF NNE HELP AND STORE AND STORE AS BOOLEAN
+        Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"\n");
+        string needHelp = Console.ReadLine();
+        Convert.ToBoolean(needHelp);
+      
+        //ASK FOR POSITIVE EXPERRIENCES AND STORE AS A STRING
+        Console.WriteLine("Where there any positive experriences that you would like to share?\n");
+        string shareExperrience=Console.ReadLine();
+
+        //ASK FOR STUDY HOURS AND STORE AS INTEGER
+        Console.WriteLine("How many hours did you study today?\n");
+        string studyHours = Console.ReadLine();
+        Convert.ToInt32(studyHours);
+
+        Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a greate day!");
+
+        //EXIT GRACEFULLY
+        Environment.Exit(0);
+      
+    }
+}
+
