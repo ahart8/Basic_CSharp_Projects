9adbb6519508e427a844a86cec96721e542f89cf
ahart8 <121808111+ahart8@users.noreply.github.com>
2023-04-10 21:24:28 -0400

    I am checking in an assignment

---
+++ BooleanOperators/BooleanOperators/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        //Console.WriteLine(true && false);
+        //Console.WriteLine(false && true);
+        //Console.WriteLine(true && true);
+        //Console.WriteLine(false && false);
+        //Console.ReadLine();
+
+        //Console.WriteLine(true || true);
+        //Console.WriteLine(true || false);
+        //Console.WriteLine(false || true);
+        //Console.WriteLine(false || false);
+
+        //Console.WriteLine(true == true);
+        //Console.WriteLine(true == false);
+        //Console.WriteLine(false == true);
+        //Console.WriteLine(false == false);
+
+        //Console.WriteLine(true != true);
+        //Console.WriteLine(true != false);
+        //Console.WriteLine(false != true);
+        //Console.WriteLine(false != false);
+
+        Console.WriteLine(true ^ true);
+        Console.WriteLine(true ^ false);
+        Console.WriteLine(false ^ true);
+        Console.WriteLine(false ^ false);
+    }
+    }
+
---
+++ CarInsuranceApproval/CarInsuranceApproval/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //Get the age and convert to an int
+        Console.Write("What is your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        //Get DUI status and convert the string to a bool
+        Console.Write("Have you ever had a DUI? Enter True or False: ");
+        bool dui = Convert.ToBoolean(Console.ReadLine());
+        
+        //Get the number of speeding tickets
+        Console.Write("How many speeding tickets do you have?: ");
+        int tickets = Convert.ToInt32(Console.ReadLine());
+
+        //Check to see if they qualify and display on the screen
+        bool qualified=(age>=15 && !dui && tickets<=3);
+        Console.WriteLine(qualified);
+        Console.ReadLine();
+        }
+    }
+
