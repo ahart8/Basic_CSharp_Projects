a5cb7fd5b64f4e2d7def9da52d55e4a144c96079
ahart8 <121808111+ahart8@users.noreply.github.com>
2023-04-15 09:24:57 -0400

    I am checking in my homework assignments

---
+++ StringsPractice/StringsPractice/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace StringsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Escape sequences
+            //string name = "Amy";
+            //string quote = " The man said,\"Hello\", Jessie\n Hello on a new line\n\t Hello on a tab";
+            //string fileName = "C:\\Users\\Amy";
+            //string fileName2 = @"C:\users\amy";
+
+            ////String functions
+            ////Contains
+            //bool trueOrFalse = name.Contains("A");
+            //trueOrFalse = name.EndsWith("b");
+
+            //int length = name.Length;
+
+            //name=name.ToUpper();
+
+            //Console.WriteLine(quote);
+            //Console.WriteLine(fileName);
+            //Console.WriteLine(fileName2);
+            //Console.WriteLine(trueOrFalse);
+            //Console.WriteLine(length);
+            //Console.WriteLine(name);
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My name is Amy");
+            Console.WriteLine(sb);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
