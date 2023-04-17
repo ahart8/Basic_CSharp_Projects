483e152ee757c48d861698cbc193c4b96ab8e074
ahart8 <121808111+ahart8@users.noreply.github.com>
2023-04-17 15:57:53 -0400

    I am checking in a coding assignment

---
+++ ArraysListsLoopsAssignment/ArraysListsLoopsAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Program
+    {
+        static void Main()
+        {
+            string[] songArray = new string[5];
+            int i;
+            do
+            {
+
+                for (i = 0; i<5; i++)
+                {
+                    Console.Write("Enter the names of 5 your favorite songs:");
+                    songArray[i] = Console.ReadLine();           
+                }
+            
+            } while (i < 5);
+       
+                foreach (string song in songArray)
+                {
+                    Console.WriteLine(song);
+                }
+        
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+                System.Environment.Exit(0);
+        }
+    }
+
---
+++ IterationPractice/IterationPractice/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+class Program
+{
+    static void Main()
+    {
+        int[] testScores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
+
+        for (int i = 0; i < testScores.Length; i++)
+        {
+            if (testScores[i] > 85)
+            {
+                Console.WriteLine("Passing test score: " + testScores[i]);
+            }
+        }
+        Console.ReadLine();
+
+        string[] names = { "Jesse", "Erik", "Daniel", "Adam" };
+
+        for (int j = 0; j < names.Length; j++)
+        {
+            Console.WriteLine(names[j]);
+
+        }
+        Console.ReadLine();
+
+
+        List<int> testScores1 = new List<int>();
+        testScores1.Add(98);
+        testScores1.Add(99);
+        testScores1.Add(81);
+        testScores1.Add(72);
+        testScores1.Add(70);
+
+        foreach(int score in testScores1)
+        {
+            if(score>85)
+            {
+                Console.WriteLine("Passing test score: " + score);
+            }
+        }
+        Console.ReadLine();
+
+
+
+        List<string> names1=new List<string> { "Jesse", "Erik", "Daniel", "Adam" };
+        foreach (string name in names1)
+        {
+            Console.WriteLine(name);
+
+        }  
+        Console.ReadLine();
+
+         List<int> testScores2 = new List<int>() { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
+        List<int> passingScores = new List<int>();
+        foreach(int score2 in testScores2)
+        {
+            if (score2>85)
+            {
+                passingScores.Add(score2);
+            }
+        }
+        Console.WriteLine(passingScores.Count);
+        Console.ReadLine();
+    }
+
+}
