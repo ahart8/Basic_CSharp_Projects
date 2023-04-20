acd865bdcb71aa7966c1c2c855a063d7c5f2b956
ahart8 <121808111+ahart8@users.noreply.github.com>
2023-04-19 20:20:14 -0400

    checking in coding assignemtn for calling methods

---
+++ ClassesAndObjectsAssignments/ClassesAndObjectsAssignments/Calculate.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading;
+
+namespace ClassesAndObjectsAssignments
+{
+    //This class contains 3 methods each taking an integer and returning an integer. 
+   public class Calculate
+    {
+        public int addFive(int num)
+        {
+            int result = 0;
+            int x = 5;
+
+            result = x + num;
+            return result;
+        }
+
+        public int multiplyByTwo(int num)
+        {
+            int result = 0;
+            int x = 2;
+
+            result = x * num;
+            return result;
+        }
+        public int modular(int num)
+        {
+            int result = 0;
+            int x = 56;
+
+            result = x % num;
+            return result;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ClassesAndObjectsAssignments/ClassesAndObjectsAssignments/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading;
+
+namespace ClassesAndObjectsAssignments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciate the calcaulate class
+            Calculate number = new Calculate();
+            int numberEntered = 0;
+
+            //Get User Input            
+            Console.Write("Enter a number: ");
+            numberEntered= Convert.ToInt32(Console.ReadLine());
+
+            //Call each method and pass the input 
+            int answer1=number.addFive(numberEntered);
+            int answer2 = number.multiplyByTwo(numberEntered);
+            int answer3 = number.modular(numberEntered);
+
+
+            //Display the returned integer to the screen
+            Console.WriteLine("Your number plus 5 is: "+ answer1);
+            Console.WriteLine("Your number times 2 is: "+ answer2);
+            Console.WriteLine("The remainer of 56 divied by your number is: "+answer3);
+
+            Console.ReadKey();
+        }
+        
+    }
+}
