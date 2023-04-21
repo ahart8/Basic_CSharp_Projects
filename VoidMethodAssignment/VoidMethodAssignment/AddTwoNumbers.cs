1465162dea28b4484eec4e984ea0e21a046864be
ahart8 <121808111+ahart8@users.noreply.github.com>
2023-04-21 17:13:33 -0400

    I am checking in my homework assignment

---
+++ VoidMethodAssignment/VoidMethodAssignment/AddTwoNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoidMethodAssignment
+{
+    class AddTwoNumbers
+    {
+        public void myMethod(int x,int y)
+        {
+           //Do a math operation on the first integer 
+           int result = x + 10; 
+           
+           //Display the second integer to the screen
+           Console.WriteLine("The value of the second integer is: "+y);
+           return;
+        }
+    }
+}
---
+++ VoidMethodAssignment/VoidMethodAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VoidMethodAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x = 5;
+            int y = 10;
+            //Instanciate the class in the main method
+            AddTwoNumbers myNumbers = new AddTwoNumbers();
+
+            //Call the method in the class specifying the parameters by name
+            myNumbers.myMethod(x,y);
+            Console.ReadLine();
+        }
+    }
+}
