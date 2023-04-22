f53cc62d30b8ddc5edad65bbb9deac893284b068
ahart8 <121808111+ahart8@users.noreply.github.com>
2023-04-21 21:54:14 -0400

    I am checking in my code so that I can get some help.

---
+++ MethodWithOptionalParameter/MethodWithOptionalParameter/OptionialParameter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodWithOptionalParameter
+{
+    class OptionialParameter
+    {
+        public int addTwoNumbers(int x, int y=8)
+        {
+           
+            int sum = x + y;
+            Console.WriteLine("The sum is: "+ sum);
+            return sum;
+        }
+    }
+}
---
+++ MethodWithOptionalParameter/MethodWithOptionalParameter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MethodWithOptionalParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+               Console.WriteLine("Please enter 2 numbers. The first an entry is not required for the second number");
+
+            try
+            {
+                Console.WriteLine("Enter the first number: ");
+                int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter the second number: ");
+                int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+                //Instanciate the (misspelled) class in the main method
+                OptionialParameter optionalParameter = new OptionialParameter();
+                optionalParameter.addTwoNumbers(firstNumber, secondNumber);
+
+              
+            }
+            catch (FormatException ex)
+            {
+                
+                Console.ReadLine();
+                
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+              
+                Console.ReadLine();
+            }
+
+        }
+    }  
+}
