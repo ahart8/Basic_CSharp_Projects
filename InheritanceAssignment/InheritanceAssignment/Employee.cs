e63f090113d6424c8b13dcd6ac3d34f97a542e40
ahart8 <121808111+ahart8@users.noreply.github.com>
2023-04-25 00:19:46 -0400

    I am checking in my homework

---
+++ InheritanceAssignment/InheritanceAssignment/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceAssignment
+{
+    class Employee : Person
+    {
+        //Set the ID property
+        int Id { get; set; }
+    }
+}
---
+++ InheritanceAssignment/InheritanceAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InheritanceAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the Employee class
+            Employee person = new Employee();
+            //Initialize the first name 
+            person.FirstName = "Sample";
+            //Initialize the last name
+            person.LastName = " Student";
+            //Call say name from the Employee object
+            person.SayName();
+        }
+    }
+}
