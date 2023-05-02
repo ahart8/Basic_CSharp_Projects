0b67a8a8d03c79c7c9b681555cdaa5637796a521
ahart8 <121808111+ahart8@users.noreply.github.com>
2023-05-02 18:00:17 -0400

    I am checking in an assignment

---
+++ DateTimeSubmissionAssignment/DateTimeSubmissionAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DateTimeSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Print the current date time to the screen
+            Console.WriteLine("The current date and time is" ,DateTime.Now);
+            //Prompt the user to imput hours to add 
+            Console.Write("Enter a number: ");
+            //Get number of hours to add to the current date time from the user
+            int numberHours =Convert.ToInt32(Console.ReadLine());
+            //Get the current date time
+            DateTime dateTime = DateTime.Now;
+            //Add the number of hour entered by the user to the current date time
+            DateTime dateTime1 = dateTime.AddHours(numberHours);
+            //Print the answer to the screen
+            Console.WriteLine("The exact time in "+numberHours + " hours will be: "+dateTime1);
+            //Make the screen pause so you can read the output
+            Console.ReadLine();
+        }
+    }
+}
