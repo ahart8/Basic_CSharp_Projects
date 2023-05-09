18ef244384e2ea814782608e2d1d1e0c54f08f5d
ahart8 <121808111+ahart8@users.noreply.github.com>
2023-05-09 16:59:28 -0400

    checking in my code

--- MVCPractice/MVCPractice/Controllers/HomeController.cs
+++ MVCPractice/MVCPractice/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVCPractice.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,39 +10,30 @@ namespace MVCPractice.Controllers
     public class HomeController : Controller
     {
         //index is the default route 
+        
         public ActionResult Index()
         {
-            //Can write out to a file
-            //string text = "hello";
-            // System.IO.File.WriteAllText(@"C:\users\amy\DeskTop\samplelog.txt",text);
 
-            // Random rnd = new Random(10);
-            // var num = rnd.Next();
-            //print number out viewbag is like a dictionary. Throw in any data type can send to the view
-            //ViewBag.RandomNumber = num;
+            User user = new User();
+            user.Id = 1;
+            user.FirstName = "Amy";
+            user.LastName = "Hart";
+            user.Age = 52;
 
-            //How to return a differnt view
-            // if (num>3)
-            // {
-            //     return View("About");
-            // }
-            //return View();
-            //Can return content as well - used sometimes when you want to return nothing. 
-            //return Content("Hello");
-           // return RedirectToAction("Contact");
-            return View();
-       }
+
+            return View(user);
+        }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-            throw new Exception("Invalid Page");
+          
             return View();
         }
 
-        public ActionResult Contact(int id=0)
+        public ActionResult Contact()
         {
-            ViewBag.Message = id;
+            ViewBag.Message = "Your application description page.";
 
             return View();
         }
